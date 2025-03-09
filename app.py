f2fca523c0a85fb7ea840f5fc2566eeae68107ac
Ayush Pandey <inbox.ayushpandey@gmail.com>
2025-03-09 17:26:57 +0530

    sia undefined fixed app.js

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from nltk.sentiment import SentimentIntensityAnalyzer
 import logging
 
+sia = SentimentIntensityAnalyzer()
 app = Flask(__name__)
 
 @app.route('/')
